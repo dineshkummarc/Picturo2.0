ec6a441a569ae4dfe2b61416924597cdcece756a
Dinesh Kumar Choudhary <mapmydestination@gmail.com>
2020-10-08 13:09:51 +0530

    update

--- bootstrap.php
+++ bootstrap.php
@@ -1,25 +1,17 @@
 <?php
 
 define('ROOT_DIR', realpath(dirname(__FILE__)) .'/');
+define('CONTENT_DIR', ROOT_DIR .'content/');
 define('CORE_DIR', ROOT_DIR .'core/');
 define('CONF_DIR', ROOT_DIR .'conf/');
-define('WWW_DIR', ROOT_DIR .'');
-define('CONTENT_DIR', WWW_DIR .'content/');
-define('THEMES_DIR', WWW_DIR .'themes/');
-define('CACHE_DIR', WWW_DIR .'cache/');
+define('THEMES_DIR', ROOT_DIR .'themes/');
+define('CACHE_DIR', ROOT_DIR .'cache/');
+define('session_domain',dirname(__FILE__));
 
-// Autoload vendors
+// Autoload
 require(ROOT_DIR .'vendor/autoload.php');
-
-function PicturoAutoload($class) {
-  spl_autoload_register(function( $class ) {
-    $classFile = str_replace( '\\', DIRECTORY_SEPARATOR, $class );
-    $classPI = pathinfo( $classFile );
-    $classPath = strtolower( $classPI[ 'dirname' ] );
-
-    include_once( $classPath . DIRECTORY_SEPARATOR . $classPI[ 'filename' ] . '.php' );
-  });
+function picturo_autoload($class) {
+  $class = strtolower($class);
+  include CORE_DIR . $class . '.php';
 }
-
-spl_autoload_register('PicturoAutoload');
-
+spl_autoload_register('picturo_autoload');

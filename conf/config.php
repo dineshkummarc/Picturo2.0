6b09835293d4c8795fc008d40b616a995adcc83a
Dinesh Kumar Choudhary <mapmydestination@gmail.com>
2020-10-09 16:40:42 +0530

    update

--- conf/config.php
+++ conf/config.php
@@ -28,5 +28,5 @@ $config['site_title'] = 'Picturo';
 $config['private'] = true;
 $config['admin_pass']['admin'] = 'sha512:1000:2Gp+SOuwkWQpMsemFvMyxfPtEfY/M77l:GrHLycEBPATCSmFCmd4onEynvfSoNR3Q';
 $config['nbOfFolders'] = 9;
-$config['theme'] = 'default'; 			// Set the theme (defaults to "default")
+$config['theme'] = 'dineshkummarc'; 			// Set the theme (defaults to "default")
 ?>

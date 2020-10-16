409bda40558ab65825ad24e923f40b980220ea79
Dinesh Kumar Choudhary <mapmydestination@gmail.com>
2020-10-16 14:05:05 +0530

    config:update

--- conf/config.php
+++ conf/config.php
@@ -26,7 +26,7 @@ $config['custom_setting'] = 'Hello'; 	// Can be accessed by {{ config.custom_set
 
 */
 $config['site_title'] = 'Picturo 2.0';
-$config['private'] = false;
+$config['private'] = true;                  // To enable private session change this to 'true'
 $config['admin_pass']['admin'] = 'sha512:1000:2Gp+SOuwkWQpMsemFvMyxfPtEfY/M77l:GrHLycEBPATCSmFCmd4onEynvfSoNR3Q';
 $config['private_pass']['toto'] = 'sha512:1000:2Gp+SOuwkWQpMsemFvMyxfPtEfY/M77l:GrHLycEBPATCSmFCmd4onEynvfSoNR3Q';
 $config['nbOfFolders'] = 1;
